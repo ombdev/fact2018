0608f60e67913a9d3386e6c1f35366c624590e75
ombdev <omontes.dev@gmail.com>
2018-01-13 19:58:22 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -32,7 +32,7 @@ def test_format_currency():
     assert HelperStr.format_currency('2555555555.59') == '2,555,555,555.59'
     assert HelperStr.format_currency('5.59') == '5.59'
     assert HelperStr.format_currency('1234.59') == '1,234.59'
-    assert HelperStr.format_currency('-2555555555.59') == '-2,555,555,555.59'
     assert HelperStr.format_currency('0.00') == '0.00'
     assert HelperStr.format_currency('-9.99') == '-9.99'
     assert HelperStr.format_currency('-.01') == '-0.01'
+    assert HelperStr.format_currency('-2555555555.59') == '-2,555,555,555.59'
