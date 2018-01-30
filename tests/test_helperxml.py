7c25c99a0a6958a6efdaa9bcb5b286070b08d921
ombdev <omontes.dev@gmail.com>
2018-01-30 00:57:36 -0600

    Update test_helperxml.py

--- tests/test_helperxml.py
+++ tests/test_helperxml.py
@@ -1,4 +1,6 @@
 from cfdiengine.misc.helperxml import HelperXml
 
 def test_run_xslt():
-    HelperXml.run_xslt('tests/sample_files/sample1.xml', 'tests/sample_files/sample1.xsl')
+    results = HelperXml.run_xslt('tests/sample_files/sample1.xml', 'tests/sample_files/sample1.xsl')
+    print(results)
+    
